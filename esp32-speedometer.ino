c2d60da1c5c0ebdba7e5378e64b42ce75beb3a17
mh-g <marcohahn@foni.net>
2023-01-08 10:31:49 +0100

    with BME280 added

--- esp32-speedometer.ino
+++ esp32-speedometer.ino
@@ -1,3 +1,4 @@
+#include "float.h"
 #include "arduinoFFT.h"
 
 #include "BluetoothSerial.h"
@@ -7,66 +8,213 @@
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
+// BME interface using https://github.com/finitespace/BME280 (BME280 by Tyler Glenn)
+#include "BME280I2C.h"
+#include "EnvironmentCalculations.h"
+#include "Wire.h"
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+#define BMESUBSAMPLE (25)
+
+// library objects
+BME280I2C bme;    // Default : forced mode, standby time = 1000 ms
+                  // Oversampling = pressure ×1, temperature ×1, humidity ×1, filter off,
 BluetoothSerial SerialBT;
 arduinoFFT FFT;
 
-int messageCounter = 0;
-int sampleCounter;
-const uint16_t samples = 1024; // max value for Arduino nano (already exceeds ram by 206 bytes, but still runs)
-double vReal[samples];
+// average environmental data
+struct envContainer {
+  float minimum;
+  float maximum;
+  float sum;
+  int samples;
+
+  public:
+    // initialize
+    envContainer () { startOver (); }
+    
+    void startOver () {
+      minimum = FLT_MAX;
+      maximum = FLT_MIN;
+      sum = 0.0;
+      samples = 0;
+    }
+
+    // process another sample
+    void addSample (float value) {
+      sum += value;
+      ++samples;
+      if (value < minimum) minimum = value;
+      if (value > maximum) maximum = value;
+    }
+
+    // return latest measurement
+    float getValue () { return sum / samples; }
+
+    // indicate if measurement is of high quality
+    bool goodQuality () { return samples >= BMESUBSAMPLE / 2; }
+};
+
+const int analogInput = 26; // used IO pin: 26 (ADC2-9)
+int messageCounter = 0; // counter for SPEED messages
+int sampleCounter; // counter for FFT input samples
+const int outputFrequency = 2; // number of FFTs per second
+const uint16_t samples = 1024; // length of FFT
+double vReal[samples]; // FFT buffer
 double vImag[samples];
-unsigned int delayUs;
-const double samplingFrequency = 1000;  // Hz
-char message[64];
-char mac[7];
+double maxAmplitude = 0.0; // maximum amplitude of the last FFT input gathering cycle
+unsigned int delayUs = 1000000 / outputFrequency / samples; // delay between two samples (design: 1024 samples per second)
+bool bmeAvailable;
+int bmeSubsampler;
+envContainer envTemperature;
+envContainer envPressure;
+envContainer envAltitude;
+char message[64]; // buffer for output message preparation
+char mac[7]; // hex representation of the last three bytes of the MAC address
 const char compile_version[] = __DATE__ " " __TIME__;
+int64_t microSecondsSinceBoot; // current time
+int64_t lastSampleTime = 0; // time of last sample
+
+// general output function, to easily tailor output to serial and/or bluetooth
+void output(char* message) {
+  Serial.print (message);
+  SerialBT.print (message);
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  // comms initialization
   Serial.begin(115200); //sets the baud rate of the UART for serial transmission
   while (!Serial);
 
-  if (SerialBT.begin("ESP32test")) {   // sets Bluetooth device name
-    Serial.println ("SerialBT.begin successful.");
+  // we can also slow down the CPU a bit to save some mA
+  setCpuFrequencyMhz(80);
+
+  if (SerialBT.begin("ESP32speedometer")) {   // sets Bluetooth device name
+    output ("SerialBT.begin successful.\n");
   }
 
-  // some initialization stuff
-  FFT = arduinoFFT();
-  delayUs = 1000; // = 1000 Hz
-  sampleCounter = 0;
-
+  // get MAC address
   const uint8_t* point = esp_bt_dev_get_address();
   sprintf(mac, "%02X%02X%02X", (int)point[3], (int)point[4], (int)point[5]);
-  
-// debug code
-  double cycles = 10.0;
+
+  // prepare FFT
+  FFT = arduinoFFT();
+  sampleCounter = 0;
+
+  // sample storage initialization
   for (int i = 0; i < samples; ++i) {
-    vReal[i] = int8_t((50.0 * (sin((i * (twoPi * cycles)) / samples))) / 2.0);
+    vReal[i] = 0.0;
     vImag[i] = 0.0;
   }
-  delay (2000);
-  sprintf (message, ">%s:SB#%03d=%s<\n", mac, 0, __DATE__ " " __TIME__);
-  SerialBT.print(message);
+
+  // initialize BME280 for environmental data and storage classes
+  Wire.begin();
+  bmeAvailable = bme.begin();
+  if (bmeAvailable) {
+    switch (bme.chipModel()) {
+      case BME280::ChipModel_BME280:
+        output("Found BME280 sensor. Success.\n");
+        break;
+      case BME280::ChipModel_BMP280:
+        output("Found BMP280 sensor. No Humidity available.\n");
+        break;
+      default:
+        output("Found UNKNOWN sensor. Error!\n");
+        bmeAvailable = false;    
+    }
+  } else {
+    output("No BME280/BMP280 found. No environmental data available.\n");
+  }
+  if (bmeAvailable) {
+    bmeSubsampler = BMESUBSAMPLE;
+    envTemperature.startOver();
+    envPressure.startOver();
+    envAltitude.startOver();
+  }
 }
 
 double majorPeak () {
   // measure the performance
+  FFT.Windowing(vReal, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD);  /* Weigh data */
   FFT.Compute(vReal, vImag, samples, FFT_FORWARD);
   FFT.ComplexToMagnitude(vReal, vImag, samples);
-  return FFT.MajorPeak(vReal, samples, samplingFrequency);
+  return FFT.MajorPeak(vReal, samples, 1.0 * outputFrequency * samples); // sampling frequency: samples filled in 1 second
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  vReal[sampleCounter] = analogRead (34);
+  // determine if enough time has passed to obtain a new sample value
+  microSecondsSinceBoot = esp_timer_get_time();
+  if (microSecondsSinceBoot >= delayUs + lastSampleTime)
+  {
+    // get next environmental data samples
+    if (bmeAvailable) {
+      --bmeSubsampler;
+      if (bmeSubsampler == 0) {
+        bmeSubsampler = BMESUBSAMPLE;
+        float temp(NAN), hum(NAN), pres(NAN);
+        
+        BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
+        BME280::PresUnit presUnit(BME280::PresUnit_Pa);
+        
+        bme.read(pres, temp, hum, tempUnit, presUnit);
+  
+        envTemperature.addSample(temp);
+//        envHumidity.addSample(hum);
+        envPressure.addSample(pres / 100.0F);
+        envAltitude.addSample(
+          Altitude(pres / 100.0f, EnvironmentCalculations::AltitudeUnit::AltitudeUnit_Meters, 
+            SEALEVELPRESSURE_HPA, temp, EnvironmentCalculations::TempUnit::TempUnit_Celsius));
+      }
+    }
+    
+    // get next hub dynamo voltage sample
+    vReal[sampleCounter] = analogRead (analogInput);
+    vImag[sampleCounter] = 0;
+    if (vReal[sampleCounter] > maxAmplitude)
+    {
+      maxAmplitude = vReal[sampleCounter];
+    }
+    sampleCounter = sampleCounter + 1;
 
-  if (sampleCounter == 999) {
-    sprintf (message, ">%s:VF#%03d=%f<\n", mac, messageCounter, majorPeak());
-    Serial.print (message);
-    SerialBT.print (message);
-    sampleCounter = -1;
-    messageCounter += 1;
-    delay(1000);
+    // calculate FFT and output all messages
+    if (sampleCounter == samples) {
+      // uptime
+      sprintf (message, ">%s:SU#%03d=%f<\n", mac, messageCounter, 0.001 * (microSecondsSinceBoot / 1000));
+      output(message);
+      // hub dynamo data
+      sprintf (message, ">%s:VF#%03d=%f<\n", mac, messageCounter, majorPeak());
+      output(message);
+      sprintf (message, ">%s:VA#%03d=%f<\n", mac, messageCounter, maxAmplitude);
+      output(message);
+      // environmental data
+      if (bmeAvailable) {
+        sprintf (message, ">%s:EL#%03d=%f<\n", mac, messageCounter, envAltitude.getValue());
+        output(message);
+        sprintf (message, ">%s:EP#%03d=%f<\n", mac, messageCounter, envPressure.getValue());
+        output(message);
+        sprintf (message, ">%s:ET#%03d=%f<\n", mac, messageCounter, envTemperature.getValue());
+        output(message);
+      }
+      // software version
+      sprintf (message, ">%s:SB#%03d=%s<\n", mac, messageCounter, __DATE__ " " __TIME__);
+      output(message);
+      
+      // increase message counter (always in the range [0..999])
+      messageCounter += 1;
+      if (messageCounter == 1000) messageCounter = 0;
+      
+      // reset sample counter and maximum amplitude
+      sampleCounter = 0;
+      maxAmplitude = 0.0;
+
+      // reset environmental values
+      bmeSubsampler = BMESUBSAMPLE;
+      envTemperature.startOver();
+      envPressure.startOver();
+      envAltitude.startOver();
+    }
+    lastSampleTime += delayUs;
+  } else { // wait a little more before trying to sample again
+    delayMicroseconds (10);
   }
-  sampleCounter = sampleCounter + 1;
 }
